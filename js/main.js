05b943405eb510e176ba95467eeebd7d197dc848
sioiyan <sioiyan3456@gmail.com>
2019-04-06 20:07:52 +0900

    copy,removeElement

--- js/main.js
+++ js/main.js
@@ -12,6 +12,12 @@
     const h2 = document.createElement('h2');
     h2.textContent = 'Sub Title';
     document.body.insertBefore(h2, p);    
+
+    // const copy = p.cloneNode(true);
+    const copy = p.cloneNode(false);
+    document.body.insertBefore(copy, h2);
+
+    document.body.removeChild(h2);
 }   
 
 
