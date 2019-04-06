4891d8cf51d471a19f546b3a7e911db215155151
sioiyan <sioiyan3456@gmail.com>
2019-04-06 20:11:55 +0900

    text,textarea

--- js/main.js
+++ js/main.js
@@ -1,23 +1,12 @@
 'use strict';
 
 {
-    const h1 = document.createElement('h1');
-    h1.textContent = 'Title';
-    document.body.appendChild(h1);
+    const text = document.querySelector('input[type="text"]');
+    const textarea = document.querySelector('textarea');
 
-    const p = document.createElement('p');
-    p.textContent = 'Hello, hello, hello...';
-    document.body.appendChild(p);
-
-    const h2 = document.createElement('h2');
-    h2.textContent = 'Sub Title';
-    document.body.insertBefore(h2, p);    
-
-    // const copy = p.cloneNode(true);
-    const copy = p.cloneNode(false);
-    document.body.insertBefore(copy, h2);
-
-    document.body.removeChild(h2);
+    // text.focus();
+    // text.select();
+    text.disabled = true;
 }   
 
 
