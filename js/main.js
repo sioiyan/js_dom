38c6cea4481da775905bdcd8584d9de681142ccd
sioiyan <sioiyan3456@gmail.com>
2019-04-06 19:58:25 +0900

    class attribute

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
 'use strict';
 
 {
-    // HTMLのカスタムデータ属性
-    const h1 = document.querySelector('h1');
+    const div = document.querySelector('div');
 
-    console.log(h1.dataset.appId);
-    h1.dataset.message = "this is custom message!";
+    div.className = 'box border-pink';
 }   
 
 
