1e792f0b4ddc3b544a2eb10b778910c5d944650f
sioiyan <sioiyan3456@gmail.com>
2019-04-06 19:38:40 +0900

    querySelector

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 'use strict';
 
 {
-   document.getElementById("target").textContent = "Changed!";
+    document.querySelector('h1').textContent = 'H1!'
+    document.querySelectorAll('li:nth-child(odd)').forEach(li => {
+        li.textContent = 'li!';
+    });
 }
 
 
